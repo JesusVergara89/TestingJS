edbfabd988c4d5bcbfe21b1d87a06d0f04512cc1
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-10-26 22:07:04 -0600

    refactor the function to return a null if b === 0

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -7,3 +7,16 @@ test('adds 1 + 2 to equal 3', () => {
 test('multiplies 1 * 2 to equal 2', () => {
   expect(math.multiply(1, 2)).toBe(2);
 });
+
+test('subtracts 1 - 2 to equal -1', () => {
+  const answer = math.subtract(1, 2);
+  expect(answer).toBe(-1);
+});
+
+test('divides 1 / 2 to equal 0.5', () => {
+  const answer1 = math.divide(1, 2);
+  expect(answer1).toBe(0.5);
+  const answer2 = math.divide(2, 0);
+  expect(answer2).toBe(null);
+});
+
